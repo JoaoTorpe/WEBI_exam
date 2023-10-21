9e84686aa24dff7c3a40f6b3a6fdee31170de906
joaoTorpe <victotorpe10@gmail.com>
2023-10-21 16:38:51 -0300

    products array created

---
+++ products.js
@@ -0,0 +1,25 @@
+let products = [
+{           
+        "id" : 1,
+        "name" : "Iphone 16",
+        "price" : 600.00
+} ,
+
+{           
+    "id" : 2,
+    "name" : "Abridor de coco",
+    "price" : 60.00
+},
+
+{           
+    "id" : 3,
+    "name" : "Harry Styles",
+    "price" : 96400.00
+},
+{           
+    "id" : 4,
+    "name" : "pc roubado do Lab I",
+    "price" : 12.50
+}
+
+]
