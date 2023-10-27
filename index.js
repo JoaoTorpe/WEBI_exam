bb7c9c821bf31c01cc9773a99c0eec6155491948
JoaoTorpe <victotorpe10@gmail.com>
2023-10-27 15:58:19 -0300

    Project partially working

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ li.innerHTML = `
             <img id = "productImg" src = "${p.imgUrl} alt = "era pra ter uma img aqui" "/>
         <h3>${p.name}</h3>
         <p>R$ ${p.price}</p>
-    <button id="addToCart" >Add to cart</button>
+    <button class="addToCart" >Add to cart</button>
     
 </div>`
 ul.appendChild(li)
@@ -21,6 +21,39 @@ ul.appendChild(li)
 
 
 const cart = document.querySelector('#cart');
-const addToCart = document.querySelector('#addToCart');
+const addToCart = document.querySelectorAll('.addToCart');
 const displayQuant = document.querySelector('#displayQuant')
+const cartProductsContainer = document.querySelector('#listContainer')
+const cartList = document.querySelector('#cartProductsList ul')
+let productsArray = [];
+let quantItems = 0;
+//Abre a lista de produtos do carrinho
+cart.addEventListener('click',()=>{
+cartProductsContainer.classList.remove('displayNone')
+cartProductsContainer.classList.add('displayOn')
+    productsArray.forEach((i)=>{
+        let li = document.createElement('li')
+        li.textContent = i
+        cartList.appendChild(li)
+
+    })
+
+})
+//Fecha a lista de produtos do carrinho
+cartProductsContainer.addEventListener('click',()=>{
+if(cartProductsContainer.classList.contains('displayOn')){
+    cartProductsContainer.classList.remove('displayOn')  
+    cartProductsContainer.classList.add('displayNone')
+}
+})
+
+//Adiciona produtos ao array
+
+addToCart.forEach((i)=>{   
+    i.addEventListener('click',(event)=>{
+     productsArray.push(event.target.parentNode.childNodes[3].innerText)   
+     quantItems += 1;
+     displayQuant.innerText = quantItems
+})  })
+
 
